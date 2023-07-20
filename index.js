c1c55102f9b3eee4bf5756165a4faea18023cbf8
Jeff Henrichs <jeffhenrichs37@gmail.com>
2023-07-20 13:07:59 -0400

    Able to get status of the field (i.e Win, Lose, Continue)

--- index.js
+++ index.js
@@ -46,6 +46,9 @@ class Field {
     let direction = this.input();
 
     console.log(this.x, this.y);
+    let prev_x = this.x; //Previous X Coord
+    let prev_y = this.y; //Previous Y Coord
+    let new_x, new_y;
 
     // Update the pathChar coordinates based on direction value
     if (direction === "A" || direction === "a") {
@@ -58,16 +61,37 @@ class Field {
       this.x--;
     }
 
-    console.log(this.x, this.y);
-    this.status();
+    new_x = this.x; //New X Coord
+    new_y = this.y; //New Y Coord
 
-    // Update the field
+    console.log("previous coords - ", prev_x, prev_y);
+    console.log("new coords - ", new_x, new_y);
+
+    let status = this.status();
+    console.log(status);
+
+    // Update the field based on the value of status
 
     this.print();
   }
 
   //Checks to see if the user Wins, Loses, or Continues
-  status() {}
+  status() {
+    if (
+      this.x < 0 ||
+      this.x > this.field[0].length ||
+      this.y < 0 ||
+      this.y > this.field.length
+    ) {
+      return "Lose - you went out of bounds.";
+    } else if (this.field[this.x][this.y] === "O") {
+      return "Lose - you fell in the hole.";
+    } else if (this.field[this.x][this.y] === "^") {
+      return "Win - you found your hat.";
+    } else {
+      return "Continue";
+    }
+  }
 }
 
 const game = new Field([
