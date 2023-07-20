6a88d03f2b6a5e043707e8fa3e3903dbf99b3f01
Jeff Henrichs <jeffhenrichs37@gmail.com>
2023-07-20 10:40:58 -0400

    added a costructor and a print method to Field class.

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const prompt = require("prompt-sync")({ sigint: true });
+// const prompt = require("prompt-sync")({ sigint: true });
 
 const hat = "^";
 const hole = "O";
-const fieldCharacter = "░";
-const pathCharacter = "*";
+const fieldChar = "░";
+const pathChar = "*";
 
-class Field {}
+class Field {
+  constructor(field) {
+    this._field = field;
+  }
+
+  print() {
+    for (let i = 0; i < this._field.length; i++) {
+      console.log(this._field[i].join(""));
+    }
+  }
+}
+
+const game = new Field([
+  [pathChar, fieldChar, fieldChar, fieldChar],
+  [fieldChar, fieldChar, hole, fieldChar],
+  [hole, fieldChar, fieldChar, hole],
+  [fieldChar, hole, fieldChar, fieldChar],
+  [fieldChar, fieldChar, hole, hat],
+]);
+
+game.print();
